70243338a50c124c1626bd5400c826c5d72bb17e
billyzou <yz3352@nyu.edu>
2019-05-01 21:08:22 -0400

    chore(router): config title for each page

--- src/router.ts
+++ src/router.ts
@@ -4,17 +4,30 @@ import Home from './views/Home.vue';
 
 Vue.use(Router);
 
-export default new Router({
+const router = new Router({
     routes: [
         {
             path: '/',
             name: 'home',
             component: Home,
+            meta: {
+                title: 'Fight The Landlord',
+            },
         },
         {
             path: '/dashboard',
             name: 'dashboard',
             component: () => import('./views/Dashboard.vue'),
+            meta: {
+                title: 'Dashboard',
+            },
         },
     ],
 });
+
+router.beforeEach((to: any, from: any, next: any) => {
+    document.title = to.meta.title;
+    next();
+});
+
+export default router;
